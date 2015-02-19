5862902f9f3984ad8729db595bb03b1553bc7994
Arthur Dickerson <arthur@5iveonefour.com>
2015-02-19 19:25:20 +0000

    adjusted offscreen menu

--- resources/views/Static/offscreenmenu.blade.php
+++ resources/views/Static/offscreenmenu.blade.php
@@ -4,18 +4,21 @@
     <ul id="osm-menu1" class="osm-vert-menu">
         <!-- <a href="/"><li class="allCaps">home</li></a>-->
         <a href="/auth/register"><li class="allCaps">Register <i class="fa fa-arrow-left"></i></li></a>
-        <a href="/practice"><li class="allCaps vertGap">practice</li></a>
+        <a href="/practice"><li class="allCaps vertGap">practice today</li></a>
         <a href="/schedule"><li class="allCaps">Schedule & Results</li></a>
-        <li class="allCaps vertGap">Track & Field News</li>
-        <a href="/contact"><li class="allCaps vertGap">Contact</li></a>
-        <li class="allCaps">Track Records @Kalaheo </li>
         <li class="allCaps">Gallery</li>
+
+
+        <li class="allCaps vertGap">Track Records @Kalaheo </li>
+        <li class="allCaps">World Records: Track & Field</li>
+
+
         <li class="allCaps vertGap">Sprints & Hurdles</li>
         <li class="allCaps">Mid/Long Distance</li>
         <li class="allCaps">Pole Vault</li>
         <li class="allCaps">Horizontal Jumps</li>
         <li class="allCaps">Strength & Power Lifting</li>
-
+        <a href="/contact"><li class="allCaps vertGap">Contact</li></a>
 
     </ul>
 
