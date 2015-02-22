d45641ef35f9feaac2de4de9d3f46c91d57d995a
Arthur Dickerson <arthur@5iveonefour.com>
2015-02-22 19:13:48 +0000

    updated practice note

--- resources/views/Practice/index.blade.php
+++ resources/views/Practice/index.blade.php
@@ -42,12 +42,8 @@
                 <h1 class="colorBlack">Practice Today:</h1>
                 <h3> {{ $today }}</h3>
 
-                <p>EVEN THOUGH IT'S RAINING: Regularly scheduled practice @Kalaheo - 3:30 - 5:30PM</p>
-                <p>Workouts may be adjusted to accommodate the wet conditions</p>
-                <p>Meet up by the lockers for announcements, and completion of team registration in the computer lab (15 mins)</p>
-                <p><b>Sprint squad:</b> bring your journals, we are lifting this afternoon</p>
-                <p><b>Mid/Distance:</b>Pre-Meet workout - run (10-15 mins and 6-8x 50meter striders)</p>
-                <p><b>UH Sunrise Meet Participants:</b> Please alert Coach Fiana, or Coach Tony if you need a ride to UH. Meet starts at 8:30AM - those in need of a ride should meet at school at 8:00AM</p>
+                <p>No practice today - it's Sunday</p>
+                
 
 
 
