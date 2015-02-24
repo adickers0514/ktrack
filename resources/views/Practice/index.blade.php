bbed6abacbdfedbc6fa92480b23efa874d8707e1
Arthur Dickerson <arthur@5iveonefour.com>
2015-02-24 20:36:02 +0000

    updated practice note for 24 Feb

--- resources/views/Practice/index.blade.php
+++ resources/views/Practice/index.blade.php
@@ -42,7 +42,11 @@
                 <h1 class="colorBlack">Practice Today:</h1>
                 <h3> {{ $today }}</h3>
 
-                <p>Regularly scheduled practice today at Kalaheo: 3:30 - 5:30. <p><b>Mid/Long Distance:</b>35-45 minutes (maintence run) + 6x100 striders, stretch, and core work</p><p><b>Sprinters: </b>Speed Work - 100/200  8x60, 200/400 4x150. Weight room - be sure to bring your journal.</p>Reminders: First OIA track meet this Saturday @Castle High School. There will be no transportation - we will meet in the stands at Castle at 12:30PM. Student athletes that have paid the team fee will receive their uniforms at the end of practice today.</p>
+                <p><b>FIRST PRACTICE AT CASTLE HIGH SCHOOL TODAY</b></p>
+
+                <p>Practice will start between 3:45 and 4:00PM - please meet in the parking lot outside the track.</p>
+                <p><b>Mid/Long Distance:</b> 1 mile warm up, active drills. <p>1000 800 600 400 2x200</p> 2 min. recovery between each run. As distance decreases, speed increases: 200's are FULL SPEED (Speed Dev workout). 1 mile cooldown. Stretch. Core work. Push-ups.</p>
+                <p><b>Sprinters:</b> 800m warmup. Stretch. Active Stretch. Speed Drills. Introduction to block starts. <p>30/30 200m repeats x 8 sets -> 200m run under :30, :30 rest -> repeat</p> 800m cool down. Stretch.</p>
 
 
 
