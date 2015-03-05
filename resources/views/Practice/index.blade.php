009a47871ddc6cbea8484aafcd4b3a459d94d527
Arthur Dickerson <arthur@5iveonefour.com>
2015-03-05 00:08:47 +0000

    updated practice note

--- resources/views/Practice/index.blade.php
+++ resources/views/Practice/index.blade.php
@@ -42,12 +42,11 @@
                 <h1 class="colorBlack">Practice Today:</h1>
                 <h3> {{ $today }}</h3>
 
-                <p><b>Regularly schedule practice: Castle High School - 4:00PM - 6:00PM</b></p>
+                <p><b>Regularly schedule practice: @Kalaheo - 3:30-5:30PM</b></p>
 
 
-                <p><b>Mid/Long Distance:</b>1 mile warm-up; active drills; 8x400
-                                            90 sec. recovery between each one; be at mile race pace; If you are shooting for a 1 mile cooldown . Stretch. Abs. Push-ups</p>
-                <p><b>Sprinters:</b> 800m Warm-up. Stretch. Speed drills. Specials: Hurdles, High-jump (with Castle), Block starts. Relay passes (4x100 steps). Tempo workout: 300m x2, 200m x2, 150x 2/3</p>
+                <p><b>Mid/Long Distance:</b>RUN: 30-40 mins. 6x100m striders. Stretch. “B” Lift Day. After practice: Foam Roll & Ice</p>
+                <p><b>Sprinters:</b> 4 laps Warm-up. Stretch. Speed drills. Specials: Hurdles, High-jump. <br />Speed workout: Jumpers/100/200 = 40m x 7, 1min rest. 20m x 7,  <br />200/400  10mins strides. 15mins easy jog </p>
 
           </div>
 
