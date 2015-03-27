f058b7a61d0fae078e94ed5e49c4795ab4c304e0
Arthur Dickerson <arthur@5iveonefour.com>
2015-03-27 23:58:20 +0000

    updated practice note

--- resources/views/Practice/index.blade.php
+++ resources/views/Practice/index.blade.php
@@ -43,21 +43,17 @@
                 <h3> {{ $today }}</h3>
 
 
-                <p>No Practice Today - School's is closed</p>
+                <p>Pre-meet Practice Today</p>
 
-                <!-- <p><b>Mid/Long Distance:</b> See Coach Fiana's <a href="http://kalaheotrack.com/distance">practice and training schedule</a></p>
+                <p><b>Mid/Long Distance:</b> See Coach Fiana's <a href="http://kalaheotrack.com/distance">practice and training schedule</a></p>
 
 
-                <p><b>Sprinters/Jumpers:</b> Warm up. Stretch. Speed Drills (Abbreviated for hurdlers - see below) </p>
+                <p><b>Sprinters/Jumpers:</b> Warm up. Stretch. Light Jog, Speed Drills. Cool down.</p>
 
-                <p>400m runners:</p>
-                <p>300m x 4 @80%, flats or spikes / your choice - 0:39-0:43s). Rest = 5mins. 4 x 150s, in spikes. Rest = 2 mins</p>
 
-                <p>100m/200m/Long jumpers runners:</p>
-                                <p>200m x 6 @80%, flats or spikes / your choice - 31.2s-36.0s. Rest = 3 mins. 4 x 150s, in spikes. Rest = 2 mins</p>
 
                 <p>Hurdlers:</p>
-                <p>300m x 3 @90% OVER HURDLES, in spikes (un-timed). Rest = 25 mins. 2 x 150s. Rest = 2 mins</p> -->
+                <p>Hurdle work as needed - 300m prep</p> -->
 
 
 
