064a2772953a227a52d4ef433aa35a8e3e3a3724
Arthur Dickerson <arthur@5iveonefour.com>
2015-04-14 23:58:18 +0000

    updated practice note for 4-14

--- resources/views/Practice/index.blade.php
+++ resources/views/Practice/index.blade.php
@@ -42,18 +42,65 @@
                 <h1 class="colorBlack">Practice Today:</h1>
                 <h3> {{ $today }}</h3>
 
-              <p>Practice @Kalaheo High School, 3:30-@4:30-5PM</p>
+              <p>Practice @Castle High School, 4:00-@5:30-6PM</p>
 
               <p><b>Mid/Long Distance:</b> See Coach Fiana's <a href="http://kalaheotrack.com/distance">practice and training schedule, updated for April.</a></p>
 
-            <p>Sprinters/Jumpers:</b> Warm up. Stretch.</p>
-            <p>Varsity Pre-Easterns Meet Prep</p>
-            <p>300m x 2 @ 50%</p>
-            <p>100m x 2 @ 70%</p>
 
-            <p>Hurdlers: hurdle work as needed</p>
-            <p>High Jumpers: haul out mats, jump as needed</p>
-            
+             <br />
+
+             <h3>Dissecting your race pace</h3>
+
+             <P>100m @15.0 = 200m @30.05</P>
+             <P>100m @14.0 = 200m @28.19</P>
+             <P>100m @13.2 = 200m @26.65</P>
+             <P>100m @12.8 = 200m @25.86</P>
+             <P>100m @12.2 = 200m @24.65</P>
+             <P>100m @12.0 = 200m @24.24</P>
+             <P>100m @11.9 = 200m @24.03</P>
+             <P>100m @11.8 = 200m @23.82</P>
+             <P>100m @11.7 = 200m @23.62</P>
+             <P>100m @11.5 = 200m @23.20</P>
+             <P>100m @11.4 = 200m @22.99</P>
+             <P>100m @11.0 = 200m @22.14</P>
+             <P>100m @10.5 = 200m @21.07</P>
+
+             <br />
+<p><b>Sprinters/Jumpers:</b> 800m jog. Warm up. Stretch. Speed Drills. </p>
+
+            <p>3 x fly 100m (20-25m fly, 100m clocked, spikes) @race pace. R=15min between reps. Rest before 200m = 20mins.</p>
+            <p>2 x 200m @race pace (spikes, from blocks) to acquire 100m splits</p>
+
+            <p>800m cool down.</p>
+
+
+<h3>Race plan: 100m</h3>
+                                         <table class="tableSettings">
+                                             <tbody>
+                                               <tr>
+                                                   <td class="captionTableColumn">0-30m</td>
+                                                   <td class="dataTableColumn">Acceleration, drive phase mechanics</td>
+                                               </tr>
+
+                                               <tr>
+                                                   <td class="captionTableColumn">30m-55m</td>
+                                                   <td class="dataTableColumn">Float - no matter what is going on around you - PATIENCE - wait for it!!!!!</td>
+                                               </tr>
+
+                                               <tr>
+                                                   <td class="captionTableColumn">55m-80m</td>
+                                                   <td class="dataTableColumn">Re-accelerate. Hips Tall. Focus on relaxed face, BIG ARMS. Emphasis on driving leg down into track. NOT on going faster.</td>
+                                               </tr>
+
+                                               <tr>
+                                                   <td class="captionTableColumn">80m-100m</td>
+                                                   <td class="dataTableColumn">Run tall, chest up, chin up, eyes forward, face RELAXED, hands RELAXED, over-emphasis arm motion - BE CONSISTENT in your stride. DO NOT BEAR DOWN to go faster.</td>
+                                               </tr>
+
+
+
+                                             </tbody>
+                                         </table>
               <!--
                               <p>Castle High School - Fast Surface Practice</p>
 
